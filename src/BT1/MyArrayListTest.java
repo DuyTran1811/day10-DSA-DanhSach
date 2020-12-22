69d5c763fc6778f03fd2bfcaa69011603fe462bf
Duy Trần <45138599+ferssken@users.noreply.github.com>
2020-12-22 20:02:45 +0700

    fix

--- src/BT1/MyArrayListTest.java
+++ src/BT1/MyArrayListTest.java
@@ -7,12 +7,6 @@ public class MyArrayListTest {
         MyList<Integer> myList = new MyList<>();
         myList.add(1);
         myList.add(5);
-        myList.add(1);
-        myList.add(9);
-        myList.add(2);
-        myList.add(7);
-        myList.add(9);
-        myList.add(10);
         System.out.println(myList.get(1));
 
     }
